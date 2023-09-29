05505e2e0ce9220299193a9f144b9763ed3147f8
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-30 01:01:35 +0900

    💄 design: layout 컴포넌트 css 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-  return <Outlet />;
+  return (
+    <div className="flex justify-center h-screen">
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
